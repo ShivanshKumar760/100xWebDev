51f1ebca2b343ae264cff488669015e870dc1a5d
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2024-10-19 02:43:54 +0530

    String methods added

--- 02.chapter3-Strings/index.js
+++ 02.chapter3-Strings/index.js
@@ -1,6 +1,6 @@
 //Declaring a string to declare a string we enclose a series of character in double or single qoute " " or ' ' 
 
-let firstName="Shiv";
+let firstName="Shiiv";
 console.log(firstName);
 
 //String Property :
@@ -14,11 +14,27 @@ console.log(firstName.length);
 //0->S
 //1->h
 //2->i
-//3->v
+//3->i
+//4->v
 console.log(firstName[2]);
 
 //String methods :since string is a class/object it has property and method(function associated with object) 
 //just  like length property which can be accessed by the dot(.) string has methods
 
-console.log(firstName.toUpperCase());
-console.log(firstName.toLowerCase());
+console.log(firstName.toUpperCase());//capatilize every character
+console.log(firstName.toLowerCase());//coverts every character to lower case 
+console.log(firstName.indexOf('i'));//give me the first index of occurance i
+console.log(firstName.lastIndexOf('i'));//gives back the index of last occurance of i
+
+//some complicated string methods
+let email="max@gmail.com";
+// let sliceEmail=email.slice(start:0,end:5)//this will slice the string till 0 to end-1 ie 0 to 4
+let sliceEmail=email.slice(0,5);
+console.log(sliceEmail);
+
+// let subString=email.substr(start:index,end:length/numberOfCharcterneeded);
+let subString=email.substr(2,5);//start from index 2 and get me 5 character after index 2
+console.log(subString);
+
+let replaceWord=email.replace('m','w');//this will replace the very first inctance of m with w
+console.log(replaceWord);
