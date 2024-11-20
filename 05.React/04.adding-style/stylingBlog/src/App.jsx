cf72795bd7eb2aae99aa53f408bf396a19355930
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2024-11-21 04:43:34 +0530

    Event understanding added

--- 05.React/04.adding-style/stylingBlog/src/App.jsx
+++ 05.React/04.adding-style/stylingBlog/src/App.jsx
@@ -2,14 +2,23 @@ import './App.css'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 function App() {
+  let greetingObject={
+    greet:"Welcome to site",
+    time:new Date().toLocaleTimeString(
+      'en-IN', 
+      { timeZone: 'Asia/Kolkata', hour12: true, hour: 'numeric', minute: 'numeric'}
+    )
+  }
+
   return (
     <>
     <Navbar/>
     <div className='greet'>
-          <h1>Hey</h1>
+          <h1>Hey,{greetingObject.greet} and time is :{greetingObject.time}</h1>
     </div>
      <div className='content'>
         <Home/>
+        <button onClick={handleClick}>Click ME</button>
     </div>
     
     </>
--- 05.React/05.adding-Event/Event/src/App.jsx
+++ 05.React/05.adding-Event/Event/src/App.jsx
@@ -9,14 +9,22 @@ function App() {
       { timeZone: 'Asia/Kolkata', hour12: true, hour: 'numeric', minute: 'numeric'}
     )
   }
+  function handleClick(eventObject)
+  {
+    console.log(eventObject);
+    console.log(eventObject.target);
+    console.log("Hello");
+    alert("on click event triggered");
+  }
   return (
     <>
     <Navbar/>
     <div className='greet'>
-    <h1>Hey,{greetingObject.greet} and time is :{greetingObject.time}</h1>
+          <h1>Hey,{greetingObject.greet} and time is :{greetingObject.time}</h1>
     </div>
      <div className='content'>
         <Home/>
+        <button onClick={handleClick}>Click ME</button>
     </div>
     
     </>
