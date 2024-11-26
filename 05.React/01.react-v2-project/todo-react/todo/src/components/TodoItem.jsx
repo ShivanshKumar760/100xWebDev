9421ffec8cb65653de69d7bb82f63080661b522c
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2024-11-26 14:56:58 +0530

    Keeper part3 form functionality added

--- 05.React/01.react-v2-project/todo-react/todo/src/components/TodoItem.jsx
+++ 05.React/01.react-v2-project/todo-react/todo/src/components/TodoItem.jsx
@@ -1,6 +1,19 @@
+import { useState } from "react";
+
 const TodoItem=(props)=>{
     const {todoItem}=props;
-    return(<li>{todoItem}</li>);
+    const [isDone,setIsDone]=useState(false);
+    function handleClick()
+    {
+        setIsDone((prevValue)=>{
+            return !prevValue;
+        });
+    }
+    return(
+    <div onClick={handleClick}>
+        <li style={{ textDecoration:isDone && "line-through"}} >{todoItem}</li>
+    </div>
+    );
 }
 
 export default TodoItem;
