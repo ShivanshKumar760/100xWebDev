3afa14c3a4d5db06036050ff96d888395576003e
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2024-12-02 00:03:12 +0530

    Error handler for task manager added

--- 03.RestAPI/task-managerAPI/starter/fullStack/index.js
+++ 03.RestAPI/task-managerAPI/starter/fullStack/index.js
@@ -3,20 +3,26 @@ import taskRouter from "./routes/task_route.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import notFound from "./middleware/not-found.js";
+import errorHandler from "./middleware/error-handler.js";
 dotenv.config();
 const app=express();
 const port=process.env.PORT||4000;
 app.use(express.static("./public"))
 app.use(express.json());
 app.use("/",taskRouter);
+app.use(errorHandler);
 
 app.use(notFound);
 
-mongoose.connect(process.env.MONGO_URL).then(()=>{
-    app.listen(port,()=>{
-        console.log(`Server is running on ${port}!`);
-    })
-}).catch((err)=>{
-    console.log(err);
-    console.log("Sorry Couldnt connect with mongo atlas");
+// mongoose.connect(process.env.MONGO_URL).then(()=>{
+//     app.listen(port,()=>{
+//         console.log(`Server is running on ${port}!`);
+//     })
+// }).catch((err)=>{
+//     console.log(err);
+//     console.log("Sorry Couldnt connect with mongo atlas");
+// })
+
+app.listen(port,()=>{
+    console.log(`Server is running on ${port}!`);
 })
