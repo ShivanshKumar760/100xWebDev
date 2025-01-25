506cc7c3991ecc73846c8b286499bbacad73e49c
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2025-01-25 06:19:15 +0530

    any type

---
+++ 07.Typesctipt/module1/04.anyType/dist/index.js
@@ -0,0 +1,41 @@
+"use strict";
+//Any type so basically if we assign a variable any type variable is actually free to take 
+//up any value of any datatype:
+let unchangeAble = 8; //the type infer by the variable will number so we cannot 
+//assign another datatype to the variable (unchangeAble)
+let changeAble; //but since here the data type is any we can provide any datatype
+//at any given point
+changeAble = 8;
+console.log(changeAble);
+changeAble = true;
+console.log(changeAble);
+//infer:
+let inferChangeAble; //this varaible will also be treated as any type 
+//cause we havent specified any value explicitly and  we have not initialized it 
+//with any value:
+inferChangeAble = 9;
+console.log(inferChangeAble);
+inferChangeAble = "Changed";
+console.log(inferChangeAble);
+//Object:
+let obj = {
+    fname: "Shivansh",
+    age: "19"
+};
+console.log(obj);
+console.log(obj.age, "type:", typeof obj.age);
+obj.age = 19;
+console.log("Value updated!");
+console.log(obj);
+console.log(obj.age, "type:", typeof obj.age);
+//Array
+let anyItems = ["Hello", 19];
+console.log(anyItems);
+anyItems.push(true);
+console.log(anyItems);
+//functions:
+function add(p1, p2) {
+    return p1 + p2;
+}
+console.log(add(1, 2));
+console.log(add("Hey-", "What's up"));
