bd76b3165b28ca41ff3833b4063d8529b39b91b5
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2025-09-18 20:42:19 +0530

    Loops -done

---
+++ 12.Python-Backend/02.DS/00.PythonBeginners/05.Loops/03.ForLoop/Sol.py
@@ -0,0 +1,9 @@
+for i in range(0,13):
+    print(i)
+
+
+#or
+
+
+for i in range(13):
+    print(i)
---
+++ 12.Python-Backend/02.DS/00.PythonBeginners/05.Loops/05.ForLoopStep/Sol.py
@@ -0,0 +1,9 @@
+for i in range(0,110,10):
+    print(i)
+
+
+# Similar approach using while loop (less preferred)
+# i=0
+# while i<110:
+#     print(i)
+#     i+=10
